e1c44a22547b5fc03b85a13aacabfa4659c69731
Harwin <harwin.sodhi33@gmail.com>
2024-09-18 23:07:16 -0700

    build script error fixed

--- index.ts
+++ index.ts
@@ -4,8 +4,14 @@ import bookRouter from "./routers/bookrouter"
 import userRouter from "./routers/userrouter"
 import "dotenv/config"
 const app = express()
+const corsConfig = {
+    credentials:true,       
+   
+}
+app.use(cors(
+    corsConfig
+))
 app.use(express.json())
-app.use(cors())
 app.use("/api/user", userRouter)
 app.use("/api/books", bookRouter)
 app.listen(process.env.PORT, () => {
